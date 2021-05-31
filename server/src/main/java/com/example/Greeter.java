5a3c90cdaac3c88f4424997dd703a75f423b6a47
manuelromana <45267973+manuelromana@users.noreply.github.com>
2021-05-31 16:34:03 +0200

    Update Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -1,15 +1,22 @@
 package com.example;
 
- 
+/**
+* classe d'accueil
+*/
 public class Greeter {
   
- /**
-  * methode d'accueil
+  /**
+  * mon constructeur
   */
   public Greeter() {
 
   }
-
+ 
+ 
+  /**
+  * @param someone nom de la personne
+  * @return message d'acceuil
+  */
   public final String greet(final String someone) {
     return String.format("Hello babacar, %s!", someone);
   }
